325169274f8a17eebfd083d3c74dc9754ccf41b4
Chris Rolando <chris.rolando@outlook.com>
2024-10-04 22:55:16 -0600

    working on lectures: 07

--- lectures/07_control.dart
+++ lectures/07_control.dart
@@ -1,5 +1,23 @@
+import 'dart:io';
 
+/**
+* clase 07: control de flujo
+*/
 
 main(){
+
+// if else
+stdout.writeln('¿Cuál es tu edad?');
+  int age = int.parse( stdin.readLineSync() ?? '0');
+  
+  if ( age >= 21 ) {
+    stdout.writeln('Ciudano');
+  } else if( age >= 18 ) {
+    stdout.writeln('Mayor de edad');
+  } else {
+    stdout.writeln('Menor de edad');
+  }
+
+  
 
 }
